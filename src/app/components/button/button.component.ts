a99409706ebeca41acf92f516f529d3cd7e4a533
mohaimenmahi <mmc.dp11@gmail.com>
2023-01-04 19:04:33 +0600

    first

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent {
+  @Input() text: string | undefined;
+  @Input() color: string | undefined;
+  @Output() btnClick = new EventEmitter();
+  
+  constructor() {
+    
+  }
+
+  onClick(): void {
+    this.btnClick.emit()
+  }
+}
